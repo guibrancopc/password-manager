4db3659875b714214425343d2d0f98b57301919d
Guilherme Carvalho <guibrancopc@gmail.com>
2024-03-31 17:50:49 -0300

    chore: remove comment

--- app/models/password.rb
+++ app/models/password.rb
@@ -2,6 +2,6 @@ class Password < ApplicationRecord
     has_many :user_passwords
     has_many :user, through: :user_passwords
 
-    encrypts :username, deterministic: true      # Add this line
-    encrypts :password                           # Add this line
+    encrypts :username, deterministic: true
+    encrypts :password
 end
